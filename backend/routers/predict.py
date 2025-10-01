5d7f0dc4ae20390aab01907114dd84db2340b1be
wessantana <weslley.vieirasantana@gmail.com>
2025-09-30 21:34:05 -0300

    feat(api): adiciona endpoint predict com metodo post

--- backend/routers/predict.py
+++ backend/routers/predict.py
@@ -1,1 +1,20 @@
 # esse arquivo será responsável pelos endpoints referentes às predições adicionadas via post
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter(prefix="/predict")
+
+class Ocorrencia(BaseModel):
+    data_ocorrencia: str
+    latitude: float
+    longitude: float
+    bairro: str
+    extra_content: dict
+
+@router.post("/")
+async def fazerPredicao(Ocorrencia: Ocorrencia):
+    
+    pass
+
+    return
